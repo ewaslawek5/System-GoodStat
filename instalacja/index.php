489816377c7d1714b26fe456430e9504ef5fb036
ewaslawek5 <slawomirpodrzycki@gmail.com>
2026-05-08 09:51:26 +0200

    Add files via upload

--- instalacja/index.php
+++ instalacja/index.php
@@ -1,90 +1,101 @@
-<link rel="stylesheet" href="css/goodstat.css">
+		<!-- Main Content -->
+		<div id="content">
 
 <?php
-	include('inc/baner_instal.php');
+//--- dolaczenie plikow
+	include('inc/menu_gora.php');
 ?>
+
+		</div>
 
 <div class="container tresc">
 
-	<div class="page-header">
-		<h1><img src="images/loading_instal.gif" /> Instalacja Systemu GoodStat</h1>
+	<div class="d-sm-flex align-items-center justify-content-between mb-4">
+		<h1 class="h1 mb-0 text-gray-800"><img src="images/favicon/android-icon-72x72.png" /> Instalacja Systemu GoodStat</h1>
 	</div>
 	
-	<div class="jumbotron border">
-		<div class="img-rounded postep_0">
-			<h2><i class="material-icons">info</i> Ważne</h2>
-			<p class="lead">Do prawidłowego działania System GoodStat wymaga Bazy Danych, więc jeśli jeszcze jej nie utworzyłeś, zrób to przed instalacją systemu, zapamiętaj też dane do jej dostępu.</p>
+	<div class="card shadow mb-4">
+		<div class="card-body">
+	
+			<div class="px-3 py-5 mb-4 bg-gradient-primary text-white">
+				<div class="rounded postep_0">
+					<h2><i class="fas fa-info-circle"></i> Ważne</h2>
+					<p class="lead">Do prawidłowego działania System GoodStat wymaga Bazy Danych, więc jeśli jeszcze jej nie utworzyłeś, zrób to przed instalacją systemu, zapamiętaj też dane do jej dostępu.</p>
+				</div>
+			</div>
 			
-		</div>
-	</div>
-	
-  <div class="row">
-	<div class="col-md-12">
-		
-	</div>
-  </div>
-
-	<div class="page-header">
-		<h2><img src="images/loading_instal.gif" /> Proces Instalacji <span></span></h2>
-	</div>
-	
-	
-<div class="row my-2">
-
-	<div class="col-sm-4">
-		<div class="card">
-			<div class="card-body">
-				<h3 class="card-title">1 - Zbieranie Informacji</h3>
-					<p class="card-text">Dane niezbędne do prawidłowego działania Systemu GoodStat.</p>
-			</div>
-		</div>
-	</div>
-
-	<div class="col-sm-4">
-		<div class="card">
-			<div class="card-body">
-				<h4 class="card-title">2 - Baza Danych</h4>
-					<p class="card-text">Czynności związane z bazą danych. Pamiętaj żeby utworzyć na swoim serwerze bazę danych dla Systemu GoodStat.</p>
-			</div>
-		</div>
-	</div>
-	
-	<div class="col-sm-4">
-		<div class="card">
-			<div class="card-body">
-				<h4 class="card-title">3 - Wynik Instalacji</h4>
-					<p class="card-text">Wynik instalacji i czynności konfiguracyjne System GoodStat.</p>
-			</div>
-		</div>
-	</div>
-
-</div>
-	
-	
-	
-	
-
-<div class="container-fluid" id="zaczynamy-guzik">
-	<div class="container">
-	  <div class="row">
-		<div class="col-md-12" style="padding: 0px 0px; margin-top: 20px;">
+		  <div class="row">
+			<div class="col-md-12">
 				
-				<a class="btn btn-primary btn-lg btn-block" href="instalacja/index_1.php" role="button">ZACZYNIJ INSTALACJĘ 0/3 <i class="material-icons">chevron_right</i></a>
+			</div>
+		  </div>
+
+			<div class="d-sm-flex align-items-center justify-content-between mb-4">
+				<h1 class="h2 mb-0 text-gray-800"><img src="images/favicon/android-icon-72x72.png" /> Proces Instalacji</h1>
+			</div>	
+			
+		<div class="row my-2">
+
+			<div class="col-sm-4">
+				<div class="card">
+					<div class="card-body">
+						<h3 class="card-title">1 - Zbieranie Informacji</h3>
+							<p class="card-text">Dane niezbędne do prawidłowego działania Systemu GoodStat.</p>
+					</div>
+				</div>
+			</div>
+
+			<div class="col-sm-4">
+				<div class="card">
+					<div class="card-body">
+						<h4 class="card-title">2 - Baza Danych</h4>
+							<p class="card-text">Czynności związane z bazą danych. Pamiętaj żeby utworzyć na swoim serwerze bazę danych dla Systemu GoodStat.</p>
+					</div>
+				</div>
+			</div>
+			
+			<div class="col-sm-4">
+				<div class="card">
+					<div class="card-body">
+						<h4 class="card-title">3 - Wynik Instalacji</h4>
+							<p class="card-text">Wynik instalacji i czynności konfiguracyjne System GoodStat.</p>
+					</div>
+				</div>
+			</div>
 
 		</div>
-	  </div>
-	</div>	
-</div>	
+			
+			
+			
+			
+
+		<div class="container-fluid" id="zaczynamy-guzik">
+			<div class="container my-4">
+			  <div class="row">
+				<div class="col-md-12" style="padding: 0px 0px; margin-top: 20px;">
+				
+					<a href="index_1.php" class="btn btn-primary btn-block btn-lg">
+						<span class="text">ZACZYNIJ INSTALACJĘ 0/3</span>
+						<span class="icon text-white-60">
+							<i class="fas fa-caret-right"></i>
+						</span>
+					</a>
+
+				</div>
+			  </div>
+			</div>	
+		</div>	
 	
 	
 
 	
 
  </div>
+ 
+ </div>
+ </div>
   
 
 
-<?php
-	include('inc/stopka_instal.php');
-?>
+
 
